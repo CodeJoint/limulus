e9c6eaf4e8d39fd6b2dc92be1a35771b9588c5ac
Holkan <HELA@Rogue1-Mac-mini.local>
2016-03-30 17:43:51 -0600

    search results resuelto, falta resolver slide left de la caja de menu en version movil

--- themes/limulus16/search.php
+++ themes/limulus16/search.php
@@ -27,24 +27,46 @@
 					<?php 
 						if ( have_posts() ) { 
 					?>
-								<h1><?php printf( _n( "%d Resultado para ", "%d Resultados para ", $total_results, 'limulus' ), $total_results ); ?>"<?php echo $query_string; ?>"</h1>
+					<h1>
+						<?php printf( _n( "%d Resultado para ", "%d Resultados para ", $total_results, 'limulus' ), $total_results ); ?>"
+						<?php echo $query_string; ?>"
+					</h1>
+								
+				</section>
+
 				<section class="cols">
+					<?php while(have_posts()):the_post(); ?>
+
+					<figure>
+						<a href="<?php the_permalink(); ?>">
+							<?php the_post_thumbnail(); ?>
+						</a>
+						<figcaption class="info_container">
+							<h3>
+								<?php the_title(); ?>
+								<span><?php the_author(); ?></span>
+							</h3>
+						</figcaption>
+					</figure>
+
+				<?php endwhile; ?>
+				</section>
+
+				<section>
+					<h1><?php } else{echo '<section class="fail">TU BÚSQUEDA <br> NO OBTUVO <br> NINGÚN <br> RESULTADO</section>'; ?></h1>
+					<span class="volver"><a href="<?php echo site_url(); ?>">ir al inicio</a></span>
+					<?php }; ?>
+
+				</section>
+
+				<!-- <section class="cols">
 					<?php
 							while ( have_posts() ) { 
 								the_post(); 
 					?>
-
-					<article class="each_post clearfix">   <!--each_search_result-->
+					<article class="each_post clearfix">  
 						<a href="<?php the_permalink(); ?>">
-							<?php 
-								if( has_post_thumbnail() ) {
-									the_post_thumbnail();
-								}else{ 
-							?>
-	
-						 	<?php echo "<img src='".THEMEPATH."/images/no_image.png' /> ";
-						} 
-						?>
+							<?php the_post_thumbnail(); ?>
 						<div class="info_container">
 
 							<h3>
@@ -54,18 +76,20 @@
 								</span>
 							</h3>
 							
-						</div><!-- info_container -->
+						</div>
 						</a>
-					</article><!-- each_serarch_result -->
+					</article>
 					
-						<h1><?php } } else { echo  '<section class="fail">TU BÚSQUEDA <br> NO OBTUVO <br> NINGÚN <br>RESULTADO</section>';?>
-							<span class="volver"><a href="<?php echo site_url(); ?>">ir al inicio</a></span>
+						<h1>
+							
+							
+							<?php }; ?>
 						</h1>
-						<?php }?>
+						
 					
-				</section>
+				</section> -->
 
-				</section>
+				
 				
 				<hr class="divider">
 				
